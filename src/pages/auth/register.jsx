dd2039f120d7166bc3cba9507b7e5664990a4fc6
putubagus13 <putu031216@gmail.com>
2023-06-10 02:09:27 +0700

    inegration registration page

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -8,9 +8,67 @@ import {HiOutlineMail} from "react-icons/hi";
 import {RiLockPasswordLine} from "react-icons/ri";
 import {FiEye, FiEyeOff} from "react-icons/fi";
 import {SiMoneygram} from "react-icons/si";
+import {MdError} from "react-icons/md";
 import Head from "next/head";
+import { withIronSessionSsr } from "iron-session/next";
+import coockieConfig from "@/helpers/cookieConfig";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+export const getServerSideProps = withIronSessionSsr(
+    async function getServerSideProps({ req, res }) {
+        const token = req.session?.token;
+
+        if (token) {
+            res.setHeader("location", "/auth/login");
+            res.statusCode = 302;
+            res.end();
+            return { prop: {token} };
+        }
+
+        return {
+            props: {
+                token: null
+            },
+        };
+    },
+    coockieConfig
+);
+
+const validationSchema = Yup.object({
+    firstName: Yup.string().required("Firstname is invalid").min(3, "First name must have at least 3 characters"),
+    lastName: Yup.string().required("Lastname is invalid").min(3, "First name must have at least 3 characters"),
+    email: Yup.string().email("Email is invalid").required("Email is invalid"),
+    password: Yup.string().min(8, "must have input 8 characters").required("Password is invalid")
+});
 
 function Register() {
+    const router = useRouter();
+    const [loading, setLoading] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState("");
+
+    const doRegister = async(values)=>{
+        setLoading(true);
+        const form = new URLSearchParams({
+            firstName: values.firstName,
+            lastName: values.lastName,
+            email: values.email, 
+            password: values.password
+        }).toString();
+
+        const {data} = await axios.post("http://localhost:3000/api/register", form);
+        console.log(data.success);
+        if(data.success === false){
+            setErrorMessage("Registration Failed");
+            setLoading(false);
+        }
+        if(data.success === true){
+            router.push("/auth/login");
+            setLoading(false);
+        }
+    };
     return (
         <>
             <Head>
@@ -34,56 +92,118 @@ function Register() {
                         </div>
                     </div>
                     <div className='flex-1 px-[8%] py-24'>
-                        <form className='flex flex-col gap-10 w-full'>
-                            <div className='lg:hidden flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</div>
-                            <h1 className='font-[500] text-primary text-2xl'>Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h1>
-                            <p className='text-secondary'>Transfering money is eassier than ever, you can access ZiPay wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
-                            <div className='w-full flex flex-col gap-6'>
-                                <div className='flex gap-3 justify-between items-center'>
-                                    <AiOutlineUser size={26}/>
-                                    <div className="flex flex-col form-control w-full">
-                                        <input type="text" placeholder="Enter your firstname" className="input input-bordered w-full" />
-                                        <label className="label hidden">
-                                            <span className="label-text-alt"></span>
-                                        </label>
+                        <Formik 
+                            initialValues={{ 
+                                email: "",
+                                firstName: "",
+                                lastName: "", 
+                                password: "" }}
+                            validationSchema = {validationSchema}
+                            onSubmit={doRegister}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+ 
+                            }) => (
+                                <form onSubmit={handleSubmit} className='flex flex-col gap-10 w-full'>
+                                    <div className='lg:hidden flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</div>
+                                    <h1 className='font-[500] text-primary text-2xl'>Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h1>
+                                    <p className='text-secondary'>Transfering money is eassier than ever, you can access ZiPay wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
+                                    {errorMessage && (<div className="flex flex-row justify-center alert alert-error shadow-lg text-white text-lg"><MdError size={30}/>{errorMessage}</div>)}
+                                    <div className='w-full flex flex-col gap-6'>
+                                        <div className='flex gap-3 justify-between items-center'>
+                                            <div className="relative flex flex-col form-control w-full">
+                                                <AiOutlineUser className={`absolute left-2 top-2 ${errors.firstName && touched.firstName && "text-error"}`} size={26}/>
+                                                <input 
+                                                    type="text" 
+                                                    name= "firstName"
+                                                    placeholder="Enter your firstname" 
+                                                    className={`border-b-2 outline-none h-12 ${errors.firstName && touched.firstName && "border-error"} bg-base-100 w-full px-12`}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.firstName}
+                                                />
+                                                {errors.firstName && touched.firstName && (
+                                                    <label className="label">
+                                                        <span className="label-text-alt text-error">{errors.firstName}</span>
+                                                    </label>)
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className='flex gap-3 justify-between items-center'>
+                                            <div className="relative flex flex-col form-control w-full">
+                                                <AiOutlineUser className={`absolute left-2 top-2 ${errors.lastName && touched.lastName && "text-error"}`} size={26}/>
+                                                <input 
+                                                    type="text" 
+                                                    name= "lastName"
+                                                    placeholder="Enter your lastname" 
+                                                    className={`border-b-2 outline-none h-12 ${errors.lastName && touched.lastName && "border-error"} bg-base-100 w-full px-12`}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.lastName}
+                                                />
+                                                {errors.lastName && touched.lastName && (
+                                                    <label className="label">
+                                                        <span className="label-text-alt text-error">{errors.lastName}</span>
+                                                    </label>)
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className='flex gap-3 justify-between items-center'>
+                                            <div className="relative flex flex-col form-control w-full">
+                                                <HiOutlineMail className={`absolute left-2 top-2 ${errors.email && touched.email && "text-error"}`} size={26}/>
+                                                <input 
+                                                    type="email" 
+                                                    name= "email"
+                                                    placeholder="Enter your email" 
+                                                    className={`border-b-2 outline-none h-12 ${errors.email && touched.email && "border-error"} bg-base-100 w-full px-12`}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.email}
+                                                />
+                                                {errors.email && touched.email && (
+                                                    <label className="label">
+                                                        <span className="label-text-alt text-error">{errors.email}</span>
+                                                    </label>)
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className='flex gap-3 justify-between items-center'>
+                                            <div className="relative flex flex-col form-control w-full">
+                                                <RiLockPasswordLine className={`absolute left-2 top-2 ${errors.password && touched.password && "text-error"}`} size={26}/>
+                                                <input 
+                                                    type="password" 
+                                                    name= "password"
+                                                    placeholder="Enter your password" 
+                                                    className={`border-b-2 outline-none h-12 ${errors.password && touched.password && "border-error"} bg-base-100 w-full px-12`}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.password}
+                                                />
+                                                {errors.password && touched.password && (
+                                                    <label className="label">
+                                                        <span className="label-text-alt text-error">{errors.password}</span>
+                                                    </label>)
+                                                }
+                                                <button type="button">
+                                                    <FiEye size={23} className="absolute top-3 right-5" />
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className='flex gap-3 justify-between items-center'>
-                                    <AiOutlineUser size={26}/>
-                                    <div className="flex flex-col form-control w-full">
-                                        <input type="text" placeholder="Enter your lastname" className="input input-bordered w-full" />
-                                        <label className="label hidden">
-                                            <span className="label-text-alt"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className='flex gap-3 justify-between items-center'>
-                                    <HiOutlineMail size={26}/>
-                                    <div className="flex flex-col form-control w-full">
-                                        <input type="text" placeholder="Enter your email" className="input input-bordered w-full" />
-                                        <label className="label hidden">
-                                            <span className="label-text-alt"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className='flex gap-3 justify-between items-center'>
-                                    <RiLockPasswordLine size={26}/>
-                                    <div className="flex flex-col form-control w-full relative">
-                                        <input type="password" placeholder="Create your password" className="input input-bordered w-full" />
-                                        <label className="label hidden">
-                                            <span className="label-text-alt"></span>
-                                        </label>
-                                        <button type="button">
-                                            <FiEye size={23} className="absolute top-3 right-5" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <button className="btn btn-primary normal-case text-white">Sign Up</button>
-                            <p className='text-primary flex gap-2 justify-center'>Already have an account? Let’s
-                                <Link href='/auth/login' className='text-accent hover:text-secondary'>Login</Link>
-                            </p>
-                        </form>
+                                    {loading ? (<button className="btn btn-primary normal-case text-white"><span className="loading loading-spinner loading-sm"></span></button>) :
+                                        (<button className="btn btn-primary normal-case text-white">Log In</button>) }
+                                    <p className='text-primary flex gap-2 justify-center'>Already have an account? Let’s
+                                        <Link href='/auth/login' className='text-accent hover:text-secondary'>Login</Link>
+                                    </p>
+                                </form>
+                            )}
+                        </Formik>
                     </div>
                 </div>
             </div>
