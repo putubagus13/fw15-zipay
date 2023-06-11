6a4d24e7b9eae41ee020a6d58e4e48e0a686f4ac
putubagus13 <putu031216@gmail.com>
2023-06-11 23:35:51 +0700

    update forgot password api

---
+++ src/pages/api/forgot-password.js
@@ -0,0 +1,18 @@
+import coockieConfig from "@/helpers/cookieConfig";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+export default withIronSessionApiRoute(
+    async function forgotRoute(req, res) {
+        const request  = await fetch("https://cute-lime-goldfish-toga.cyclic.app/auth/forgot-password", {
+            method: "POST",
+            body: new URLSearchParams(req.body).toString(),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        });
+
+        const respons = await request.json();
+        return res.json(respons);
+    },
+    coockieConfig
+);
