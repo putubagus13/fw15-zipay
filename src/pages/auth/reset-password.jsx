5a4a363688e7ab8b5318a081748e5ebd8a3e8f6e
putubagus13 <putu031216@gmail.com>
2023-06-11 23:39:21 +0700

    integration reset password

--- src/pages/auth/reset-password.jsx
+++ src/pages/auth/reset-password.jsx
@@ -7,10 +7,81 @@ import {HiOutlineMail} from "react-icons/hi";
 import {RiLockPasswordLine} from "react-icons/ri";
 import {FiEye, FiEyeOff} from "react-icons/fi";
 import {SiMoneygram} from "react-icons/si";
-import {TbCodeAsterix} from "react-icons/tb";
 import Head from "next/head";
+import {MdError} from "react-icons/md";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import { withIronSessionSsr } from "iron-session/next";
+import coockieConfig from "@/helpers/cookieConfig";
+import axios from "axios";
+import { useRouter } from "next/router";
+import react from "react";
+
+export const getServerSideProps = withIronSessionSsr(
+    async function getServerSideProps({ req, res }) {
+        const token = req.session?.token;
+
+        if (token) {
+            res.setHeader("location", "/home");
+            res.statusCode = 302;
+            res.end();
+            return { prop: {token} };
+        }
+
+        return {
+            props: {
+                token: null
+            },
+        };
+    },
+    coockieConfig
+);
+
+const validationSchema = Yup.object({
+    email: Yup.string().email("Email is invalid").required("Email is invalid"),
+    password: Yup.string().min(8, "must have input 8 characters").required("Password is invalid"),
+    confirmPassword: Yup.string()
+        .required("Confirm password is empty !")
+        .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
 
 function ResetPassword() {
+    const router = useRouter();
+    const [loading, setLoading] = React.useState(false);
+    const [successMessage, setSuccessMessage] = react.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState("");
+    const [showEye, setShowEye] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+
+    const doLogin = async(values)=>{
+        setErrorMessage("");
+        setLoading(true);
+        const form = new URLSearchParams({
+            email: values.email, 
+            password: values.password,
+            confirmPassword: values.confirmPassword
+        }).toString();
+
+        const {data} = await axios.post("http://localhost:3000/api/reset-password", form);
+
+        if(data.message === "auth_reset_password_not_match"){
+            setErrorMessage("You have not made a reset request");
+            setLoading(false);
+        }
+        if(data.success === true){
+            setLoading(false);
+            setSuccessMessage(true);
+        }
+    };
+
+    const doShowEye = ()=>{
+        setShowEye(!showEye);
+    };
+
+    const openEye = ()=>{
+        setOpen(!open);
+    };
     return (
         <>
             <Head>
@@ -33,50 +104,104 @@ function ResetPassword() {
                     </div>
                 </div>
                 <div className='flex-1 px-[8%] py-24'>
-                    <form className='flex flex-col gap-10 w-full'>
-                        <div className='lg:hidden flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</div>
-                        <h1 className='font-bold text-primary text-2xl'>Did You Forgot Your Password? Don’t Worry, You Can Reset Your Password In a Minutes.</h1>
-                        <p className='text-secondary'>Now you can create a new password for your FazzPay account. Type your password twice so we can confirm your new passsword.</p>
-                        <div className='w-full flex flex-col gap-6'>
-                            <div className='flex gap-3 justify-between items-center'>
-                                <HiOutlineMail size={26}/>
-                                <div className="flex flex-col form-control w-full">
-                                    <input type="text" placeholder="Enter your email" className="input input-bordered w-full" />
-                                    <label className="label hidden">
-                                        <span className="label-text-alt"></span>
-                                    </label>
+                    <Formik 
+                        initialValues={{ 
+                            email: "", 
+                            password: "",
+                            confirmPassword: "" }}
+
+                        validationSchema = {validationSchema}
+                        onSubmit={doLogin}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+ 
+                        }) => (
+                            <form onSubmit={handleSubmit} className='flex flex-col gap-10 w-full'>
+                                <div className='lg:hidden flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</div>
+                                <h1 className='font-[500] text-primary text-2xl'>Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h1>
+                                <p className='text-secondary'>Transfering money is eassier than ever, you can access ZiPay wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
+                                {errorMessage && (<div className="flex flex-row justify-center alert alert-error shadow-lg text-white text-lg"><MdError size={30}/>{errorMessage}</div>)}
+                                <div className='w-full flex flex-col gap-6'>
+                                    <div className="relative flex flex-col form-control w-full">
+                                        <HiOutlineMail className={`absolute left-2 top-2 ${errors.email && touched.email && "text-error"}`} size={26}/>
+                                        <input 
+                                            type="email" 
+                                            name= "email"
+                                            placeholder="Enter your email" 
+                                            className={`border-b-2 outline-none h-12 ${errors.email && touched.email && "border-error"} bg-base-100 w-full px-12`}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.email}
+                                        />
+                                        {errors.email && touched.email && (
+                                            <label className="label">
+                                                <span className="label-text-alt text-error">{errors.email}</span>
+                                            </label>)
+                                        }
+                                    </div>
+                                    <div className='flex gap-3 justify-between items-center'>
+                                        <div className="relative flex flex-col form-control w-full">
+                                            <RiLockPasswordLine className={`absolute left-2 top-2 ${errors.password && touched.password && "text-error"}`} size={26}/>
+                                            <input 
+                                                type={showEye ? "text" : "password"} 
+                                                name= "password"
+                                                placeholder="Enter your password" 
+                                                className={`border-b-2 outline-none h-12 ${errors.password && touched.password && "border-error"} bg-base-100 w-full px-12`}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.password}
+                                            />
+                                            {errors.password && touched.password && (
+                                                <label className="label">
+                                                    <span className="label-text-alt text-error">{errors.password}</span>
+                                                </label>)
+                                            }
+                                            <button type="button" onClick={doShowEye}>
+                                                {showEye ? (<FiEyeOff size={23} className="absolute top-3 right-5" />) :
+                                                    (<FiEye size={23} className="absolute top-3 right-5" />)
+                                                }
+                                                    
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className='flex gap-3 justify-between items-center'>
+                                        <div className="relative flex flex-col form-control w-full">
+                                            <RiLockPasswordLine className={`absolute left-2 top-2 ${errors.confirmPassword && touched.confirmPassword && "text-error"}`} size={26}/>
+                                            <input 
+                                                type={open ? "text" : "password"} 
+                                                name= "confirmPassword"
+                                                placeholder="Enter your password" 
+                                                className={`border-b-2 outline-none h-12 ${errors.confirmPassword && touched.confirmPassword && "border-error"} bg-base-100 w-full px-12`}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.confirmPassword}
+                                            />
+                                            {errors.confirmPassword && touched.confirmPassword && (
+                                                <label className="label">
+                                                    <span className="label-text-alt text-error">{errors.confirmPassword}</span>
+                                                </label>)
+                                            }
+                                            <button type="button" onClick={openEye}>
+                                                {open ? (<FiEyeOff size={23} className="absolute top-3 right-5" />) :
+                                                    (<FiEye size={23} className="absolute top-3 right-5" />)
+                                                }
+                                                    
+                                            </button>
+                                        </div>
+                                    </div>
+                                    {successMessage && <p>Password reset was successful, please <Link href='/auth/login' className='text-accent hover:text-primary text-right'>Login</Link> again</p>}
                                 </div>
-                            </div>
-                            <div className='flex gap-3 justify-between items-center'>
-                                <RiLockPasswordLine size={26}/>
-                                <div className="flex flex-col form-control w-full relative">
-                                    <input type="password" placeholder="Create your new password" className="input input-bordered w-full" />
-                                    <label className="label hidden">
-                                        <span className="label-text-alt"></span>
-                                    </label>
-                                    <button type="button">
-                                        <FiEye size={23} className="absolute top-3 right-5" />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className='flex gap-3 justify-between items-center'>
-                                <RiLockPasswordLine size={26}/>
-                                <div className="flex flex-col form-control w-full relative">
-                                    <input type="password" placeholder="Enter confirm new password" className="input input-bordered w-full" />
-                                    <label className="label hidden">
-                                        <span className="label-text-alt"></span>
-                                    </label>
-                                    <button type="button">
-                                        <FiEye size={23} className="absolute top-3 right-5" />
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <button className="btn btn-primary normal-case text-white">Reset Password</button>
-                        <p className='text-primary flex gap-2 justify-center'>Reset Password Success? Let’s
-                            <Link href='/auth/login' className='text-accent hover:text-secondary'>Login</Link>
-                        </p>
-                    </form>
+                                {loading ? (<button className="btn btn-primary normal-case text-white"><span className="loading loading-spinner loading-sm"></span></button>) :
+                                    (<button className="btn btn-primary normal-case text-white">Reset</button>) }
+                            </form>
+                        )}
+                    </Formik>
                 </div>
             </div>
         </>
