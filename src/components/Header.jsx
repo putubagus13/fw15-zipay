eac4ac08df71e661e687950c4d397148a5715bbe
putubagus13 <putu031216@gmail.com>
2023-06-12 17:17:51 +0700

    update: conditional rendering profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 import {SiMoneygram} from "react-icons/si";
 import Image from "next/image";
-import Icon from "../assets/pexels-pixabay-220453.jpg";
+import User from "@/assets/user.png";
 import {MdNotificationsNone} from "react-icons/md";
 import {FiMenu} from "react-icons/fi";
 import Link from "next/link";
-import axios from "axios";
+import http from "@/helpers/http";
 
 function Header({token}) {
     const [profile, setProfile] = React.useState({});
 
     React.useEffect(()=>{
         async function getProfile(){
-            const {data} = await axios.get("https://cute-lime-goldfish-toga.cyclic.app/profile", {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
+            const {data} = await http(token).get("/profile");
             setProfile(data.results);
         }
         getProfile();
 
-    },[]);
+    },[token]);
     return (
         <header className="flex justify-between items-center px-[8%] py-6 bg-white rounded-b-3xl shadow-lg">
             <Link href="/home" className='flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</Link>
             <div className="hidden lg:flex justify-center items-center gap-3">
                 <Link href="/profile" className="w-16 h-16 overflow-hidden rounded-2xl">
-                    <Image className="object-cover" src={Icon} alt=""/>
+                    {profile?.picture ? (<Image width={150} height={150} className="object-fit" src={user.picture} alt="userImage"/>) 
+                        : (<Image className="object-fit" src={User} alt="user"/>) }
                 </Link>
                 <div className="flex flex-col">
                     <p className="font-bold text-primary">{profile?.fullName}</p>
