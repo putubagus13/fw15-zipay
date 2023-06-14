07abba77a7f028261b047f829cb07011b2b2667a
putubagus13 <putu031216@gmail.com>
2023-06-14 08:59:53 +0700

    fix: change username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,23 @@ import {MdNotificationsNone} from "react-icons/md";
 import {FiMenu} from "react-icons/fi";
 import Link from "next/link";
 import http from "@/helpers/http";
+import { useDispatch, useSelector } from "react-redux";
+import {setProfile} from "@/redux/reducers/profile";
 
 function Header({token}) {
-    const [profile, setProfile] = React.useState({});
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.profile.data);
+
+    const getData = React.useCallback(async()=>{
+        const {data} = await http(token).get("/profile");
+        dispatch(setProfile(data.results));
+
+    }, [token, dispatch]);
 
     React.useEffect(()=>{
-        async function getProfile(){
-            const {data} = await http(token).get("/profile");
-            setProfile(data.results);
-        }
-        getProfile();
+        getData();
 
-    },[token]);
+    },[getData]);
     return (
         <header className="flex justify-between items-center px-[8%] py-6 bg-white rounded-b-3xl shadow-lg">
             <Link href="/home" className='flex font-bold text-2xl text-primary'><SiMoneygram size={35}/><span className='text-3xl text-accent'>ZI</span>Pay</Link>
@@ -28,7 +33,7 @@ function Header({token}) {
                 </Link>
                 <div className="flex flex-col">
                     <p className="font-bold text-primary">{profile?.fullName}</p>
-                    <p>{profile?.username}</p>
+                    <p>{profile?.email}</p>
                 </div>
                 <MdNotificationsNone className="text-accent hover:text-primary" size={30}/>
             </div>
