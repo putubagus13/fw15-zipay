181a79d8b0a2d4e07c8367576c4b74472ac445c4
putubagus13 <putu031216@gmail.com>
2023-06-14 09:00:44 +0700

    fix: update integration top up

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Icon from "../../assets/pexels-pixabay-220453.jpg";
-import {MdOutlineLogout} from "react-icons/md";
+import {MdOutlineLogout, MdError} from "react-icons/md";
+import {BsCheckCircleFill} from "react-icons/bs";
 import {LuLayoutDashboard} from "react-icons/lu";
 import {AiOutlineArrowUp, AiOutlinePlus, AiOutlineUser, AiOutlineArrowDown} from "react-icons/ai";
 import Link from "next/link";
+import User from "@/assets/user.png";
 
 import { withIronSessionSsr } from "iron-session/next";
 import coockieConfig from "@/helpers/cookieConfig";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Header from "@/components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import http from "@/helpers/http";
+import { setProfile } from "@/redux/reducers/profile";
 
 export const getServerSideProps = withIronSessionSsr(
     async function getServerSideProps({ req, res }) {
@@ -44,6 +48,42 @@ export const getServerSideProps = withIronSessionSsr(
 
 function Homepage({token}) {
     const router = useRouter();
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.profile.data);
+    const [amount, setAmount] = React.useState("");
+    const [errorMessage, setErrorMessage] = React.useState("");
+    const [successMessage, setSuccessMessage] = React.useState(false);
+
+    const [transaction, setTransaction] = React.useState([]);
+    const dataTrs = React.useCallback(async()=>{
+        const {data} = await http(token).get("/transactions", {params: {limit: 4}});
+        setTransaction(data.results);
+    }, [token]);
+
+    React.useEffect(()=>{
+        dataTrs();
+    }, [dataTrs]);
+
+    
+    const doTopUp = async()=>{
+        try {
+            console.log(amount);
+            const form = new URLSearchParams({
+                amount
+            });
+            const {data} = await http(token).post("/transactions/topup", form);
+            console.log(data.results);
+            if(data.results){
+                dispatch(setProfile(data.results));
+                setSuccessMessage(true);
+            }
+            
+        } catch (error) {
+            return setErrorMessage("Top-UP failed, try again");
+        }
+
+    };
+
     const doLogout = async()=>{
         await axios.get("/api/logout");
         router.replace("/auth/login");
@@ -76,7 +116,23 @@ function Homepage({token}) {
                             <div className="bg-white text-white w-1">.</div>
                             <div className="flex gap-6 text-accent hover:text-primary">
                                 <AiOutlinePlus size={30}/>
-                                <Link href="" className="font-[500] text-xl">Top Up</Link>
+                                <button onClick={()=>window.my_modal_1.showModal()} className="font-[500] text-xl">Top Up</button>
+                                <dialog id="my_modal_1" className="modal">
+                                    <form method="dialog" className="modal-box flex flex-col gap-6">
+                                        <h3 className="font-bold text-primary text-lg">Enter Top-UP</h3>
+                                        <p className="py-4 pr-28 text-left">Enter the amount of money, and click submit.</p>
+                                        <div className="flex flex-col gap-2 justify-center items-center w-full">
+                                            {successMessage && <BsCheckCircleFill className="text-success" size={60}/>}
+                                            {errorMessage && (<div className="flex flex-row justify-center alert alert-error shadow-lg text-white text-lg"><MdError size={30}/>{errorMessage}</div>)}
+                                            <p className="py-4 text-left">Min top-up is Rp20.000</p>
+                                            <input onChange={(e)=> setAmount(e.target.value)} type="text" placeholder="Enter nominal Top-UP" className="input input-bordered w-full" />
+                                        </div>
+                                        <div className="modal-action flex gap-2">
+                                            <button onClick={doTopUp} disabled={amount < 20000} type="submit" className="btn btn-primary w-full h-full lg:w-36 normal-case rounded-xl text-md">Continue</button>
+                                            <button type="submit" className="btn btn-accent w-full h-full lg:w-36 normal-case rounded-xl text-md">Cancel</button>
+                                        </div>
+                                    </form>
+                                </dialog>
                             </div>
                         </div>
                         <div className="flex items-center gap-10">
@@ -109,7 +165,7 @@ function Homepage({token}) {
                     <div className="flex justify-between items-center bg-secondary w-full rounded-3xl px-[6%] py-10">
                         <div className="flex flex-col gap-5">
                             <label className="text-xl text-accent">Balance</label>
-                            <label className="font-bold text-4xl text-white">Rp <span>50.000.000</span></label>
+                            <label className="font-bold text-4xl text-white">{profile?.balance ? `Rp${Number(profile?.balance).toLocaleString("id")}` : "Rp0"}</label>
                             <label className="text-xl text-accent">+62081234567</label>
                         </div>
                         <div className="flex flex-col gap-3">
@@ -168,62 +224,73 @@ function Homepage({token}) {
                                 <Link href="/history" className="font-[500] text-secondary hover:text-warning text-xl">See All</Link>
                             </div>
                             <div className="flex flex-col gap-6">
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-3">
-                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                            <Image className="object-cover" src={Icon} alt=""/>
+                                {transaction.map(items => {
+                                    return(
+                                        <div key={`transaction${items.id}`} className="flex justify-between items-center">
+                                            {items.type === "TOP-UP" && 
+                                            <>
+                                                <div className="flex gap-3">
+                                                    <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                        <Link href="" className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                            {items.recipient.picture ? (<Image width={150} height={150} className="object-fit" src={items.recipient.picture} alt="userImage"/>) 
+                                                                : (<Image className="object-fit" src={User} alt="user"/>) }
+                                                        </Link>
+                                                    </div>
+                                                    <div className="flex flex-col gap-1">
+                                                        <label className="font-bold text-xl">{items.recipient.fullName || items.recipient.email}</label>
+                                                        <label className="text-xl">Top-Up</label>
+                                                    </div>
+                                                </div>
+                                            </>}
+                                            {items.type === "TRANSFER" && 
+                                            <>
+                                                {items.recipient.id !== profile.id && 
+                                                <>
+                                                    <div className="flex gap-3">
+                                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                            <Link href="" className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                                {items.recipient.picture ? (<Image width={150} height={150} className="object-fit" src={items.recipient.picture} alt="userImage"/>) 
+                                                                    : (<Image className="object-fit" src={User} alt="user"/>) }
+                                                            </Link>
+                                                        </div>
+                                                        <div className="flex flex-col gap-1">
+                                                            <label className="font-bold text-xl">{items.recipient.fullName || items.recipient.email}</label>
+                                                            <label className="text-xl">Outcome</label>
+                                                        </div>
+                                                    </div>
+                                                </>}
+                                                {items.recipient.id === profile.id && 
+                                                <>
+                                                    <div className="flex gap-3">
+                                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                            <Link href="" className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                                {items.sender.picture ? (<Image width={150} height={150} className="object-fit" src={items.sender.picture} alt="userImage"/>) 
+                                                                    : (<Image className="object-fit" src={User} alt="user"/>) }
+                                                            </Link>
+                                                        </div>
+                                                        <div className="flex flex-col gap-1">
+                                                            <label className="font-bold text-xl">{items.sender.fullName || items.sender.email}</label>
+                                                            <label className="text-xl">Income</label>
+                                                        </div>
+                                                    </div>
+                                                </>}
+                                            </>}
+                                            {items.type === "TOP-UP" && 
+                                            <div>
+                                                <label className="font-bold text-2xl text-success">+{items.amount && `Rp${Number(items.amount).toLocaleString("id")}`}</label>
+                                            </div>}
+                                            {items.type === "TRANSFER" && (items.recipient.id === profile.id ? 
+                                                (<div>
+                                                    <label className="font-bold text-success text-2xl text-success">+{items.amount && `Rp${Number(items.amount).toLocaleString("id")}`}</label>
+                                                </div>) : 
+                                                (<div>
+                                                    <label className="font-bold text-error text-2xl">-{items.amount && `Rp${Number(items.amount).toLocaleString("id")}`}</label>
+                                                </div>))
+                                            }
                                         </div>
-                                        <div className="flex flex-col gap-1">
-                                            <label className="font-bold text-xl">Hendri</label>
-                                            <label className="text-xl">Accept</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <label className="font-bold text-2xl text-success">+Rp<span>13.000.000</span></label>
-                                    </div>
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-3">
-                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                            <Image className="object-cover" src={Icon} alt=""/>
-                                        </div>
-                                        <div className="flex flex-col gap-1">
-                                            <label className="font-bold text-xl">Luthfi</label>
-                                            <label className="text-xl">Transfer</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <label className="font-bold text-2xl text-error">+Rp<span>35.000</span></label>
-                                    </div>
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-3">
-                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                            <Image className="object-cover" src={Icon} alt=""/>
-                                        </div>
-                                        <div className="flex flex-col gap-1">
-                                            <label className="font-bold text-xl">Netflix</label>
-                                            <label className="text-xl">Accept</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <label className="font-bold text-2xl text-success">+Rp<span>100.000</span></label>
-                                    </div>
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-3">
-                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                            <Image className="object-cover" src={Icon} alt=""/>
-                                        </div>
-                                        <div className="flex flex-col gap-1">
-                                            <label className="font-bold text-xl">Cuakil</label>
-                                            <label className="text-xl">Transfer</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <label className="font-bold text-2xl text-error">+Rp<span>550.000</span></label>
-                                    </div>
-                                </div>
+                                    );
+                                })}
+                                
                             </div>
                         </div>
                     </div>
