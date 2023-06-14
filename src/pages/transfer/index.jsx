f7723b6ef78b4de34aec5c7f91f72d76e1f8ec2a
putubagus13 <putu031216@gmail.com>
2023-06-14 09:03:04 +0700

    fix: itegration

--- src/pages/transfer/index.jsx
+++ src/pages/transfer/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Icon from "../../assets/pexels-pixabay-220453.jpg";
+import User from "@/assets/user.png";
 import {MdOutlineLogout} from "react-icons/md";
 import {LuLayoutDashboard, LuSearch} from "react-icons/lu";
 import {AiOutlineArrowUp, AiOutlinePlus, AiOutlineUser} from "react-icons/ai";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import {setRecipient as setRecipientAction } from "@/redux/reducers/transfer";
 
 import { withIronSessionSsr } from "iron-session/next";
 import coockieConfig from "@/helpers/cookieConfig";
 import axios from "axios";
 import Header from "@/components/Header";
+import http from "@/helpers/http";
+import { useDispatch, useSelector } from "react-redux";
 
 export const getServerSideProps = withIronSessionSsr(
     async function getServerSideProps({ req, res }) {
@@ -36,8 +39,39 @@ export const getServerSideProps = withIronSessionSsr(
 );
 
 function History({token}) {
-
     const router = useRouter();
+    const dispatch = useDispatch();
+    const [recipient, setRecipient] = React.useState({});
+    const [search, setSearch] = React.useState("");
+    const [page, setPage] = React.useState({});
+
+    const getUsers = React.useCallback(async(page=1, search="", limit=4, )=>{
+        const {data} = await http(token).get("/users", {params: {
+            page,
+            search,
+            limit
+        }});
+        setRecipient(data);
+        setPage(data.pageInfo);
+    },[token]);
+
+    React.useEffect(()=>{
+        getUsers();
+    }, [getUsers]);
+
+    React.useEffect(()=>{
+        getUsers(1, search);
+
+    }, [search, getUsers]);
+
+    const recipientRedux = useSelector(state => state.transfer.user);
+
+    React.useEffect(()=>{
+        if(recipientRedux){
+            router.push("/transfer/amount");
+        }
+    },[recipientRedux]);
+
     const doLogout = async()=>{
         await axios.get("/api/logout");
         router.replace("/auth/login");
@@ -46,7 +80,7 @@ function History({token}) {
     return (
         <div>
             <Head>
-                <title>ZIPay | Home</title>
+                <title>ZIPay | Transfer</title>
             </Head>
             <Header token={token} />
             <div className="flex gap-6 px-[6%] py-16">
@@ -102,76 +136,34 @@ function History({token}) {
                 <div className="flex flex-col w-full rounded-3xl shadow-lg bg-white p-6 gap-16">
                     <div className="flex flex-col w-full gap-2 relative">
                         <label className="font-[500] text-primary text-xl">Search Receiver</label>
-                        <input type="text" placeholder="Search" className="input w-full pl-16" />
+                        <input onChange={(e)=> setSearch(e.target.value)} type="text" placeholder="Search" className="input w-full pl-16" />
                         <LuSearch className="absolute top-11 left-5 text-primary" size={27}/>
                     </div>
                     <div className="flex flex-col gap-10">
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
+                        {recipient.results && 
+                        <>
+                            {recipient.results.map(items =>(
+                                <div onClick={()=> dispatch(setRecipientAction(items))} key={`recipient-list${items.id}`} href="/transfer/1" className="cursor-pointer flex items-center shadow-md p-2 rounded-xl hover:bg-neutral">
+                                    <div className="flex gap-3">
+                                        <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                            <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                                {items.picture ? (<Image width={150} height={150} className="object-fit" src={items.picture} alt="userImage"/>) 
+                                                    : (<Image className="object-fit" src={User} alt="user"/>) }
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col gap-1">
+                                            <label className="font-bold text-xl">{items.fullName}</label>
+                                            <label className="text-xl">{items.email}</label>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
-                        <Link href="/transfer/1" className="flex items-center shadow-lg p-2 rounded-xl hover:bg-neutral">
-                            <div className="flex gap-3">
-                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </div>
-                                <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
-                                    <label className="text-xl">+62083673786</label>
-                                </div>
-                            </div>
-                        </Link>
+                            ))}
+                        </>}
+                    </div>
+                    <div className="flex gap-6 justify-center w-full">
+                        <button onClick={()=> getUsers(page.page - 1)} disabled={page.page <= 1} className="btn btn-secondary shadow-md normal-case">Prev</button>
+                        <label className="flex justify-center items-center font-[500] text-md text-secondary">{page.page} of {page.totalPage}</label>
+                        <button onClick={()=> getUsers(page.page + 1)} disabled={page.page === page.totalPage} className="btn btn-secondary shadow-md normal-case">Next</button>
                     </div>
                 </div>
             </div>
