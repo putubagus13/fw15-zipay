9c74acf0ee0029c1c8c321aab875d8ca45f584b8
putubagus13 <putu031216@gmail.com>
2023-06-14 09:02:19 +0700

    fix: itegration success transaction

--- src/pages/transfer/[id].jsx
+++ src/pages/transfer/[id].jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Icon from "../../assets/pexels-pixabay-220453.jpg";
-import {MdOutlineLogout} from "react-icons/md";
+import {MdOutlineLogout, MdOutlineFileDownload} from "react-icons/md";
 import {LuLayoutDashboard} from "react-icons/lu";
 import {AiOutlineArrowUp, AiOutlinePlus, AiOutlineUser} from "react-icons/ai";
-import {LuPencil} from "react-icons/lu";
+import {BsCheckCircleFill, BsFillXCircleFill } from "react-icons/bs";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import User from "@/assets/user.png";
 
 import { withIronSessionSsr } from "iron-session/next";
 import coockieConfig from "@/helpers/cookieConfig";
 import axios from "axios";
 import Header from "@/components/Header";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import http from "@/helpers/http";
+import moment from "moment";
 
 export const getServerSideProps = withIronSessionSsr(
     async function getServerSideProps({ req, res }) {
@@ -26,6 +29,7 @@ export const getServerSideProps = withIronSessionSsr(
                 prop: {}
             };
         }
+        
         return {
             props: {
                 token,
@@ -36,9 +40,27 @@ export const getServerSideProps = withIronSessionSsr(
     coockieConfig
 );
 
-function TransferByUserId({token}) {
-    const [nominal, setNominal] = React.useState(0);
+function Success({token}) {
+    const {query: {id}} = useRouter();
+    const profile = useSelector(state=>state.profile.data);
+    const [dataRecipient, setDataRecipient] = React.useState({});
+    const [dataPicture, setDataPicture] = React.useState({});
     const router = useRouter();
+    const balanceLeft = profile.balance - dataRecipient.amount;
+
+    const getDataStatus = React.useCallback(async()=>{
+        const {data} = await http(token).get("/transactions/"+id);
+        console.log(data);
+        if(data.results){
+            setDataRecipient(data.results);
+            setDataPicture(data.results.recipient);
+        }
+    },[id, token]);
+
+    React.useEffect(()=>{
+        getDataStatus();
+    },[getDataStatus]);
+
 
     const doLogout = async()=>{
         await axios.get("/api/logout");
@@ -48,7 +70,7 @@ function TransferByUserId({token}) {
     return (
         <div>
             <Head>
-                <title>ZIPay | Transfer</title>
+                <title>ZIPay | Status</title>
             </Head>
             <Header token={token} />
             <div className="flex gap-6 px-[6%] py-16">
@@ -102,37 +124,68 @@ function TransferByUserId({token}) {
                     </div>
                 </aside>
                 <div className="flex flex-col w-full rounded-3xl shadow-lg bg-white p-6 gap-6">
-                    <div className="flex flex-col w-full gap-2">
-                        <label className="font-[500] text-primary text-xl">Transfer Money</label>
-                    </div>
                     <div className="flex flex-col gap-10">
+
+                        {dataRecipient && <div className="w-full flex flex-col gap-5 justify-center items-center py-10">
+                            <BsCheckCircleFill size={80} className="text-success"/>
+                            <label className="font-bold text-xl text-secondary">Transfer Success</label>
+                        </div>}
+
+                        {!dataRecipient && <div className="w-full flex flex-col gap-5 justify-center items-center py-10">
+                            <BsFillXCircleFill size={80} className="text-error"/>
+                            <label className="font-bold text-xl text-secondary">Transfer Failed</label>
+                            <label className="text-md text-accent px-[30%] text-center">We can’t transfer your money at the moment, we recommend you to check your internet connection and try again.</label>
+                        </div>}
+                        
+                        <div className="flex flex-col gap-6">
+                            <div className="flex items-center shadow-lg p-4 rounded-xl">
+                                <div className="flex flex-col gap-2">
+                                    <label className="text-md">Amount</label>
+                                    <label className="font-[500] text-2xl">{dataRecipient.amount && `Rp${Number(dataRecipient.amount).toLocaleString("id")}`}</label>
+                                </div>
+                            </div>
+                            <div className="flex items-center shadow-lg p-4 rounded-xl">
+                                <div className="flex flex-col gap-2">
+                                    <label className="text-md">Balance Left</label>
+                                    <label className="font-[500] text-2xl">{balanceLeft && `Rp${Number(balanceLeft).toLocaleString("id")}`}</label>
+                                </div>
+                            </div>
+                            <div className="flex items-center shadow-lg p-4 rounded-xl">
+                                <div className="flex flex-col gap-2">
+                                    <label className="text-md">Date & Time</label>
+                                    <label className="font-[500] text-2xl">{moment(dataRecipient.createdAt).format("MMMM Do, YYYY - HH.mm")}</label>
+                                </div>
+                            </div>
+                            <div className="flex items-center shadow-lg p-4 rounded-xl">
+                                <div className="flex flex-col gap-2">
+                                    <label className="text-md">Note</label>
+                                    <label className="font-[500] text-xl">{dataRecipient.notes}</label>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <label className="font-[500] text-primary text-xl">Transfer To</label>
                         <div className="flex items-center shadow-lg p-2 rounded-xl">
                             <div className="flex gap-3">
-                                <Link href="/profile/" className="w-16 h-16 overflow-hidden rounded-2xl">
-                                    <Image className="object-cover" src={Icon} alt=""/>
-                                </Link>
+                                <div className="w-16 h-16 overflow-hidden rounded-2xl">
+                                    {dataPicture.picture ? (<Image width={150} height={150} className="object-fit" src={dataPicture.picture} alt="userImage"/>) 
+                                        : (<Image className="object-fit" src={User} alt="user"/>) }
+                                </div>
                                 <div className="flex flex-col gap-1">
-                                    <label className="font-bold text-xl">Cuakly</label>
+                                    <label className="font-bold text-xl">{dataPicture.fullName}</label>
                                     <label className="text-xl">+62083673786</label>
                                 </div>
                             </div>
                         </div>
-                        <p className="text-primary pr-[60%]">Type the amount you want to transfer and then press continue to the next steps.</p>
-                        <div className="flex flex-col gap-10 justify-center items-center w-full px-[15%]">
-                            <input 
-                                className="text-center outline-none font-bold text-6xl text-accent" 
-                                type="text" 
-                                value={nominal} 
-                            />
-                            <label className="font-bold text-md text-primary">Rp120.000 Available</label>
-                            <div className="relative">
-                                <input className="border-b-2 w-full h-10 border-b-accent hover:border-b-primary outline-none px-10" placeholder="Add some note"></input>
-                                <LuPencil className="absolute text-accent bottom-2 left-2" size={22}/>
-                            </div>
-                        </div>
-                        <div className="flex w-full h-12 justify-center lg:justify-end">
-                            <button className="btn btn-accent hover:btn-primary w-full h-full lg:w-36 normal-case text-xl rounded-xl">Continue</button>
-                        </div>
+
+                        {dataRecipient && <div className="flex gap-4 w-full h-16 justify-center lg:justify-end">
+                            <Link href="/home" className="btn btn-primary w-full h-full normal-case rounded-2xl text-xl">Back to Home</Link>
+                            <button className="btn btn-accent hover:btn-primary w-full h-full normal-case text-xl rounded-xl"><MdOutlineFileDownload size={25}/>Download PDF</button>
+                        </div>}
+
+                        {!dataRecipient && <div className="flex gap-4 w-full h-16 justify-center lg:justify-end">
+                            <button className="btn btn-primary h-full w-full normal-case rounded-2xl text-xl flex gap-2">Try Again</button>
+                        </div>}
                     </div>
                 </div>
             </div>
@@ -147,4 +200,4 @@ function TransferByUserId({token}) {
     );
 };
 
-export default TransferByUserId;
+export default Success;
