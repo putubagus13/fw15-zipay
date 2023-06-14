4a635cc9450a1e4ff6cd0dc0ceee6b76a175294a
putubagus13 <putu031216@gmail.com>
2023-06-14 09:01:29 +0700

    fix: edit picture

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -7,12 +7,15 @@ import {AiOutlineArrowUp, AiOutlinePlus, AiOutlineUser} from "react-icons/ai";
 import {FiArrowRight} from "react-icons/fi";
 import User from "@/assets/user.png";
 import Link from "next/link";
+import { setProfile } from "@/redux/reducers/profile";
 
 import { withIronSessionSsr } from "iron-session/next";
 import coockieConfig from "@/helpers/cookieConfig";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Header from "@/components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import http from "@/helpers/http";
 
 export const getServerSideProps = withIronSessionSsr(
     async function getServerSideProps({ req, res }) {
@@ -26,16 +29,10 @@ export const getServerSideProps = withIronSessionSsr(
                 prop: {}
             };
         }
-        const {data} = await axios.get("https://cute-lime-goldfish-toga.cyclic.app/profile", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
         
         return {
             props: {
-                token,
-                user: data.results
+                token
             },
         };
         
@@ -43,11 +40,50 @@ export const getServerSideProps = withIronSessionSsr(
     coockieConfig
 );
 
-function Profile({token, user}) {
+function Profile({token}) {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.profile.data);
+    const [pictureURI, setPictureURI] = React.useState("");
+    const [selectedPicture, setSelectedPicture] = React.useState({});
     const router = useRouter();
     const doLogout = async()=>{
         await axios.get("/api/logout");
         router.replace("/auth/login");
+    };
+
+    const fileToDataUrl = (file) => {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            setPictureURI(reader.result);
+        });
+        reader.readAsDataURL(file);
+    };
+    
+    const changePicture = (e) => {
+        const file = e.target.files[0];
+        setSelectedPicture(file);
+        fileToDataUrl(file);
+    };
+
+    const doEditPicture = async (values) => {
+        const form = new FormData();
+        Object.keys(values).forEach((key) => {
+            if (values[key]) {
+                form.append(key, values[key]);
+            }
+        });
+        if(selectedPicture) {
+            form.append("picture", selectedPicture);
+        }
+        if(token) {
+            const { data } = await http(token).patch("/profile", form, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            dispatch(setProfile(data.results));
+            setPictureURI("");
+        }
     };
 
     return (
@@ -88,32 +124,32 @@ function Profile({token, user}) {
                             </div>
                         </div>
                     </div>
-                    {/* <div className="flex items-center gap-10">
-                        <div className="bg-white text-white w-1">.</div>
-                        <div className="flex gap-6 text-accent hover:text-primary">
-                            <MdOutlineLogout size={30}/>
-                            <button onClick={()=>window.my_modal_5.showModal()} className="font-[500] text-xl">Log Out</button>
-                            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                                <form method="dialog" className="modal-box">
-                                    <h3 className="font-bold text-lg">Log Out</h3>
-                                    <p className="py-4">Are you sure you want to logout?</p>
-                                    <div className="modal-action">
-                                        <button type="button" onClick={doLogout} className="btn btn-error">Ok</button>
-                                        <button className="btn">Close</button>
-                                    </div>
-                                </form>
-                            </dialog>
-                        </div>
-                    </div> */}
                 </aside>
                 <div className="flex flex-col w-full rounded-3xl shadow-lg bg-white py-16 px-[10%] gap-6">
                     <div className="flex flex-col gap-12">
                         <div className="flex flex-col justify-center items-center gap-3">
-                            <div className="w-20 h-20 overflow-hidden rounded-2xl">
-                                {user?.picture ? (<Image width={150} height={150} className="object-fit" src={user.picture} alt="userImage"/>) 
-                                    : (<Image className="object-fit" src={User} alt="user"/>) }
-                            </div>
-                            <button type="button" className="flex gap-2 items-center justify-center font-[500] text-accent hover:text-primary"><LuPencil size={17}/>Edit</button>
+                            {pictureURI && <div className="w-20 h-20 overflow-hidden rounded-2xl">
+                                <Image className="object-fit" src={pictureURI} width={150} height={150} alt=""/>
+                            </div>}
+                            {!pictureURI && <div className="w-20 h-20 overflow-hidden rounded-2xl">
+                                {user.picture === null ? (<Image className="object-fit" src={User} alt="user"/>) 
+                                    : (<Image width={150} height={150} className="object-fit" src={user.picture} alt="userImage"/>) }
+                            </div>}
+                            <label className="flex gap-2 items-center justify-center font-[500] text-accent hover:text-primary">
+                                {!pictureURI ? (
+                                    <>
+                                        <input
+                                            name="picture"
+                                            type="file"
+                                            className="hidden"
+                                            onChange={changePicture}
+                                        />
+                                        <LuPencil size={17} />
+                                        Edit
+                                    </>
+                                ) : null}
+                            </label>
+                            {pictureURI && <button onClick={doEditPicture} className="font-[500] text-accent hover:text-primary" type="button">Save</button>}
                             <div className="flex flex-col items-center">
                                 <p className="font-bold text-primary">{user.fullName}</p>
                                 <p>+62081234567</p>
